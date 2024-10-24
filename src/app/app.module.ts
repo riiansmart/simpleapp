9c3f4e02f37f1c3f4740f60bd37b48b43193b76b
riiansmart <rismart1221@outlook.com>
2024-10-24 12:46:25 -0700

    added shop form in shop.component, added FormGroup and onSubmit method for form submissions in shop.component.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ShopComponent } from './shop/shop.component';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { ShopComponent } from './shop/shop.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    ReactiveFormsModule,
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [
     provideClientHydration()
--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-shop',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrl: './shop.component.css'
 })
 export class ShopComponent {
+  question = "What's your name?";
+  answer = "Michael Smart";
 
+// Initializing a FormGroup to handle form data. The form has a single control named 'answer'.
+appForm = new FormGroup({
+  answer: new FormControl('') // Creates a new form control for inputting an answer, initialized with an empty string.
+});
+
+// Method to handle form submission.
+onSubmit(data: Partial<{ answer: string | null }>) { // Accepts partial form data with 'answer' which may be a string or null.
+  this.answer = data.answer!; // Updates the 'answer' property with the submitted form data. The '!' is a TypeScript non-null assertion operator, asserting that `data.answer` is not null.
+  console.log('Your name is:', this.answer); // Logs the submitted answer to the console.
+  } 
 }
