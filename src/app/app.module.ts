4103f4cc55bce5fc08af19616055badaf1808c5a
riiansmart <rismart1221@outlook.com>
2024-10-24 12:47:25 -0700

    generated info component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ShopComponent } from './shop/shop.component';
+import { InfoComponent } from './info/info.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ShopComponent
+    ShopComponent,
+    InfoComponent
   ],
   imports: [
     BrowserModule,
