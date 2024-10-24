fbc15ecc6ac8cc03c75e1c3091ad3b272223f667
riiansmart <rismart1221@outlook.com>
2024-10-24 13:11:50 -0700

    added InfoComponent logic in info.component

--- src/app/info/info.component.ts
+++ src/app/info/info.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-info',
   templateUrl: './info.component.html',
   styleUrl: './info.component.css'
 })
-export class InfoComponent {
+export class InfoComponent implements OnInit {
+  @Input() name!: string;
+  quantity = 0;
+  products: string[] = [];
+  selectedProduct = "Star Wars";
 
+  constructor() { }
+
+  ngOnInit() {
+      this.quantity = 0;
+      this.products = ["Star Wars", "The Empire Strikes Back", "Return of the Jedi"];
+      this.selectedProduct = "Star Wars";
+  }
 }
